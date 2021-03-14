65f15bb752848f1b2ed10a35d355d29ff78c6a4f
ErdemAkz <akz.erdem@gmail.com>
2021-03-14 17:07:07 +0000

    Set the port number from the environment variable

--- app/src/main/java/test/App.java
+++ app/src/main/java/test/App.java
@@ -3,20 +3,16 @@
  */
 package test;
 
-import java.util.ArrayList;
-
-
-
-
-import java.util.HashMap;
-import java.util.Map;
-
-
-
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
@@ -26,6 +22,15 @@ public class App {
     
 
     public static void main(String[] args) {
+
+        Logger logger = LogManager.getLogger(App.class);
+
+        int port = Integer.parseInt(System.getenv("PORT"));
+        port(port);
+        logger.error("Current port number:" + port);
+
+        
+
         ArrayList<Integer> list = new ArrayList<>();
         for(int i=0; i<10;i++){
             list.add (0); 
